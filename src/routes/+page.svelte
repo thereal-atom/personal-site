479b495844bf41ab5adb9f882a774fa89c798a15
thereal-atom <oscarfal2006@gmail.com>
2024-11-12 14:39:50 +0000

    proper responsiveness for skills and socials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
         class="absolute w-screen h-full z-10 pointer-events-none"
         style="background-size: 64px; background-repeat: repeat; background-image: url('/fuzzy.png'); opacity: 0.06;"
     ></div>
-    <div class="flex flex-row items-center justify-between w-full h-[900px] z-[100] p-8 max-xl:flex-col max-xl:h-full max-sm:p-4">
+    <div class="flex flex-row items-center justify-between w-full h-[900px] p-8 max-xl:flex-col max-xl:h-full max-sm:p-4">
         <div class="flex flex-col justify-between w-[74.5%] h-full max-xl:w-full">
             <div class="flex flex-row justify-between w-full h-[32%] max-xl:h-[250px] max-md:h-auto">
                 <div class="box w-auto h-full aspect-square relative max-md:hidden">
@@ -27,7 +27,7 @@
             </div>
             <div class="flex flex-col justify-between h-[66.5%] max-xl:flex max-xl:flex-col-reverse">
                 <div class="flex flex-row justify-between h-[49%] max-xl:h-[200px] max-xl:mt-4 max-md:mt-0 max-md:flex-col max-md:h-full">
-                    <div class="box w-[49.5%] h-full relative overflow-hidden max-md:w-full max-md:h-[250px] max-sm:h-[200px] max-md:mt-4">
+                    <div class="box w-[49.5%] h-full relative overflow-hidden max-md:w-full max-md:h-[250px] max-md:mt-4 max-sm:h-[200px] max-xs:h-[160px]">
                         <img
                             class="absolute -top-8 opacity-60 hover:opacity-100 hover:scale-105 transition-all max-2xl:static max-2xl:w-auto max-2xl:h-full max-2xl:min-w-full max-2xl:min-h-full max-2xl:object-cover"
                             src="/cribgoch.jpg"
@@ -36,7 +36,7 @@
                         <p class="absolute right-6 top-6 pr-3 pl-2.5 py-2 bg-[#1B1B1B] text-xs border border-white border-opacity-5 rounded-md">🏔️ I also like climbing mountains</p>
                         <p class="absolute left-8 bottom-4 text-[8px]">Crib Goch, Wales</p>
                     </div>
-                    <div class="box w-[49.5%] h-full relative overflow-hidden max-md:w-full max-md:h-[250px] max-sm:h-[200px] max-md:mt-4">
+                    <div class="box w-[49.5%] h-full relative overflow-hidden max-md:w-full max-md:h-[250px] max-md:mt-4 max-sm:h-[200px] max-xs:h-[160px]">
                         <img
                             class="absolute -top-4 opacity-60 hover:opacity-100 hover:scale-105 transition-all max-2xl:static max-2xl:w-auto max-2xl:h-full max-2xl:min-w-full max-2xl:min-h-full max-2xl:object-cover"
                             src="/corfu.jpg"
@@ -46,10 +46,10 @@
                         <p class="absolute left-8 bottom-4 text-[8px]">Corfu, Greece</p>
                     </div>
                 </div>
-                <div class="flex flex-row justify-between h-[49%] max-xl:flex-row-reverse max-xl:h-[250px] max-xl:mt-4 max-md:h-[150px]">
-                    <div class="grid grid-cols-2 gap-2 w-auto h-full aspect-square max-xl:ml-4">
+                <div class="flex flex-row justify-between h-[49%] max-xl:flex-row-reverse max-xl:h-[270px] max-xl:mt-4 max-md:flex-col-reverse max-md:h-full max-md:w-full">
+                    <div class="grid grid-cols-2 gap-4 w-auto h-full aspect-square max-xl:ml-4 max-md:grid-cols-4 max-md:flex max-md:flex-row max-md:w-full max-md:h-auto max-md:aspect-auto max-md:ml-0 max-md:mt-4 max-xs:gap-2">
                         <a
-                            class="box-custom-bg bg-[#151515] w-auto h-full aspect-square p-9 max-md:p-5 hover:bg-black transition-all"
+                            class="box-custom-bg bg-[#151515] w-auto h-full aspect-square p-9 max-md:p-5 hover:bg-black transition-all max-md:w-full max-md:h-auto"
                             href={constants.socials.x.url}
                             target="_blank"
                         >
@@ -60,7 +60,7 @@
                             />
                         </a>
                         <a
-                            class="box-custom-bg bg-[#151515] w-auto h-full aspect-square p-9 max-md:p-5 hover:bg-[#24292e] transition-all"
+                            class="box-custom-bg bg-[#151515] w-auto h-full aspect-square p-9 max-md:p-5 hover:bg-[#24292e] transition-all max-md:w-full max-md:h-auto"
                             href={constants.socials.github.url}
                             target="_blank"
                         >
@@ -72,7 +72,7 @@
                             />
                         </a>
                         <a
-                            class="box-custom-bg bg-[#151515] w-auto h-full aspect-square p-9 max-md:p-5 hover:bg-[#0a66c2] transition-all"
+                            class="box-custom-bg bg-[#151515] w-auto h-full aspect-square p-9 max-md:p-5 hover:bg-[#0a66c2] transition-all max-md:w-full max-md:h-auto"
                             href={constants.socials.linkedin.url}
                             target="_blank"
                         >
@@ -84,7 +84,7 @@
                             />
                         </a>
                         <a
-                            class="box-custom-bg bg-[#151515] w-auto h-full aspect-square p-9 max-md:p-5 hover:bg-[#c71610] transition-all"
+                            class="box-custom-bg bg-[#151515] w-auto h-full aspect-square p-9 max-md:p-5 hover:bg-[#c71610] transition-all max-md:w-full max-md:h-auto"
                             href={constants.socials.email.url}
                             target="_blank"
                         >
@@ -96,50 +96,64 @@
                             />
                         </a>
                     </div>
-                    <div class="box w-full h-full flex flex-col ml-4 p-8 overflow-hidden max-xl:ml-0">
-                        <h1 class="text-3xl font-semibold mt-1">Skills</h1>
-                        <div class="flex flex-row mt-8 w-full overflow-hidden max-xl:grid-cols-4 max-xl:mt-4">
-                            <div class="w-24 h-24 min-w-[96px] p-6 bg-[#101010] rounded-[16px] max-xl:w-16 max-xl:h-16 max-xl:min-w-[64px] max-xl:p-4">
+                    <div class="box w-full h-full flex flex-col ml-4 p-8 overflow-hidden max-xl:ml-0 max-xl:py-6 max-md:py-4 max-md:px-6">
+                        <h1 class="text-3xl font-semibold max-md:text-xl">Skills</h1>
+                        <div class="flex flex-row flex-wrap mt-8 w-full overflow-hidden max-xl:grid-cols-4 max-xl:mt-4 max-md:mt-2">
+                            <div class="skill-container">
                                 <img
+                                    class="skill-icon"
                                     src="/skills/typescript.svg"
                                     alt="typescript"
                                 />
+                                <p class="skill-text">Typescript</p>
                             </div>
-                            <div class="w-24 h-24 min-w-[96px] p-6 ml-2 bg-[#101010] rounded-[16px] max-xl:w-16 max-xl:h-16 max-xl:min-w-[64px] max-xl:p-4">
+                            <div class="skill-container">
                                 <img
+                                    class="skill-icon"
                                     src="/skills/sveltekit.svg"
                                     alt="sveltekit"
                                 />
+                                <p class="skill-text">SvelteKit</p>
                             </div>
-                            <div class="w-24 h-24 min-w-[96px] p-6 ml-2 bg-[#101010] rounded-[16px] max-xl:w-16 max-xl:h-16 max-xl:min-w-[64px] max-xl:p-4">
+                            <div class="skill-container">
                                 <img
+                                    class="skill-icon"
                                     src="/skills/nodejs.svg"
                                     alt="nodejs"
                                 />
+                                <p class="skill-text">Node.js</p>
                             </div>
-                            <div class="w-24 h-24 min-w-[96px] p-6 ml-2 bg-[#101010] rounded-[16px] max-xl:w-16 max-xl:h-16 max-xl:min-w-[64px] max-xl:p-4">
+                            <div class="skill-container">
                                 <img
-                                    src="/skills/tailwindcss.svg"
-                                    alt="tailwindcss"
-                                />
-                            </div>
-                            <div class="w-24 h-24 min-w-[96px] p-6 ml-2 bg-[#101010] rounded-[16px] max-xl:w-16 max-xl:h-16 max-xl:min-w-[64px] max-xl:p-4">
-                                <img
+                                    class="skill-icon"
                                     src="/skills/rust.svg"
                                     alt="rust"
                                 />
+                                <p class="skill-text">Rust</p>
                             </div>
-                            <div class="w-24 h-24 min-w-[96px] p-6 ml-2 bg-[#101010] rounded-[16px] max-xl:w-16 max-xl:h-16 max-xl:min-w-[64px] max-xl:p-4">
+                            <div class="skill-container">
                                 <img
-                                    src="/skills/python.svg"
-                                    alt="python"
+                                    class="skill-icon"
+                                    src="/skills/tailwindcss.svg"
+                                    alt="tailwindcss"
                                 />
+                                <p class="skill-text">Tailwind</p>
                             </div>
-                            <div class="w-24 h-24 min-w-[96px] p-6 ml-2 bg-[#101010] rounded-[16px] max-xl:w-16 max-xl:h-16 max-xl:min-w-[64px] max-xl:p-4">
+                            <div class="skill-container">
                                 <img
+                                    class="skill-icon"
                                     src="/skills/html.svg"
                                     alt="html"
                                 />
+                                <p class="skill-text">HTML</p>
+                            </div>
+                            <div class="skill-container">
+                                <img
+                                    class="skill-icon"
+                                    src="/skills/python.svg"
+                                    alt="python"
+                                />
+                                <p class="skill-text">Python</p>
                             </div>
                         </div>
                     </div>
@@ -155,10 +169,22 @@
 
 <style lang="postcss">
     .box {
-        @apply rounded-[32px] bg-[#151515] border border-white border-opacity-5;
+        @apply rounded-[32px] bg-[#161616] border border-white border-opacity-5;
     }
 
     .box-custom-bg {
         @apply rounded-[32px] border border-white border-opacity-5 max-md:rounded-[16px];
+    }
+
+    .skill-container {
+        @apply flex flex-row items-center h-fit p-4 bg-[#242424] rounded-[8px] mr-2 mb-2 max-lg:p-3 max-md:p-2 max-xs:mr-1 max-xs:mb-1;
+    }
+
+    .skill-icon {
+        @apply w-8 h-8 max-lg:w-6 max-lg:h-6 max-xs:w-4 max-xs:h-4;
+    }
+
+    .skill-text {
+        @apply ml-2 font-medium max-md:ml-1 max-md:text-xs max-xs:text-[10px];
     }
 </style>
